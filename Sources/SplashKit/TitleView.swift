c343e89272f5b19069993357ab0ecc987cdf5bc1
Ben Robinson <benrobinsonhk@gmail.com>
2020-07-17 22:54:46 +0100

    Made title image optional

--- Sources/SplashKit/TitleView.swift
+++ Sources/SplashKit/TitleView.swift
@@ -5,18 +5,20 @@ import SwiftUI
 struct TitleView: View {
     
     let title: (line1: String, line2: String)
-    let image: Image
+    let image: Image?
     let mainColor: Color
     
     var body: some View {
         
         VStack {
             
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 180, alignment: .center)
-                .accessibility(hidden: true)
+            if let image = image {
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, alignment: .center)
+                    .accessibility(hidden: true)
+            }
             
             Text(title.line1)
                 .font(.system(size: 36, weight: .black, design: .rounded))
