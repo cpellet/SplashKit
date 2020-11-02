90c186541ea958aad8c30fdfb8f4c9a1fd3c03e5
Ben Robinson <benrobinsonhk@gmail.com>
2020-11-02 20:01:40 +0000

    Added spacer to scroll view to fix #1.

--- Sources/SplashKit/SplashScreenView.swift
+++ Sources/SplashKit/SplashScreenView.swift
@@ -23,57 +23,9 @@ public struct SplashScreenView: View {
                     .padding(.top)
                     .padding(.top)
                 InfoContainerView(content: content.infoContent, mainColor: content.tintColor)
+                Spacer(minLength: 80)
             }
         }
-
-        
-//        ZStack {
-//
-//            ScrollView {
-//
-//                VStack(alignment: .center) {
-//
-//
-//
-//                    Spacer(minLength: 80)
-//
-//                }
-//
-//            }
-//
-//            VStack {
-//
-//                Spacer()
-//
-//                Button {
-//                    content.onButtonTap()
-//                    presentationMode.wrappedValue.dismiss()
-//                } label: {
-//                    Text(content.buttonText)
-//                        .foregroundColor(.white)
-//                        .font(.headline)
-//                        .padding()
-//                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-//                        .background(RoundedRectangle(cornerRadius: 15, style: .continuous).fill(content.tintColor))
-//                        .padding(.bottom)
-//                }
-//                .padding(.horizontal)
-//                .padding(.top)
-//                .padding(.top)
-//                .background(
-//                    LinearGradient(
-//                        gradient: Gradient(colors: [Color(.systemBackground),
-//                                                    Color(.systemBackground),
-//                                                    Color(.systemBackground),
-//                                                    Color(.systemBackground).opacity(0.0)]),
-//                        startPoint: .center,
-//                        endPoint: .top)
-//                        .edgesIgnoringSafeArea(.bottom)
-//                )
-//
-//            }
-//
-//        }
         
     }
     
